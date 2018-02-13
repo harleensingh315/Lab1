edf0643476f5a86c8e19621c49e0f264c4617b85
harleen singh <harleensingh315@gmail.com>
2018-02-13 11:02:50 -0500

    first commit

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab1.Models;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+
+        IList<products> productsList = new List<products>() {
+                    new products(){  productNames="Laptop" },
+                    new products(){  productNames="Tablet" },
+                    new products(){  productNames="Desktop"}
+        };
+                   
+               
+        // GET: Products
+        public ActionResult Index()
+        {
+            ViewBag.Allproducts = productsList;
+
+            return View();
+        }
+
+        // GET: Details
+        public ActionResult Details(string value)
+        {
+            string x = "You selected product: [" + value +"]";
+            ViewBag.selecteditem = x;
+            return View();
+        }
+    }
+}
---
+++ Lab1/Models/products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class products
+    {
+        public string productNames { get; set; }
+
+        
+
+        }
+    }
